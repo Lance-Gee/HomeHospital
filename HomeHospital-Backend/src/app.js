560e541d18d3ed856ec5b1cfe312f2271f6cda06
Lance Gee <Lancegee.apps@gmail.com>
2022-05-13 20:10:39 -0600

    fixes

--- HomeHospital-Backend/src/app.js
+++ HomeHospital-Backend/src/app.js
@@ -1,22 +1,22 @@
-import express from 'express'
-import ENV from './configure/configure.js'
-import { RunApp } from './loaders/loaders.js'
+import express from "express";
+import ENV from "./configure/configure.js";
+import { RunApp } from "./loaders/loaders.js";
 
-const PORT = process.env.PORT || ENV.APP_PORT
+const PORT = process.env.PORT || ENV.APP_PORT;
 
 // Create Express App Instance
-const app = express()
+const app = express();
 
 // Run the application
 try {
-	await RunApp(app)
+  await RunApp(app);
 } catch (error) {
-	console.error(`${new Date()}\tError:  ${error.message}`)
-	console.log('A Major error happened and the Server Stopped!')
-	process.exit(1)
+  console.error(`${new Date()}\tError:  ${error.message}`);
+  console.log("A Major error happened and the Server Stopped!");
+  process.exit(1);
 }
 
 // Set the app to listen on environment Port
 app.listen(PORT, () => {
-	console.log(`🚀 Server is running on http://localhost:${PORT}`)
-})
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
